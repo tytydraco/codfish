df9f122ace999f54ea73cfc433004e2296f54a4d
Tyler Nijmeh <tylernij@gmail.com>
2021-06-17 17:13:57 -0700

    Greatly clean up migrate method, move logic to main
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,53 +1,132 @@
 import adb
 import log
 import args
-from migrate import Migrate
+import migrate
+import itertools
 import sys
 
 
-if __name__ == '__main__':
-    args.parse_args()
+# Bail if ADB is missing
+def assert_adb_exists():
     if adb.sanity_check() is False:
         log.err('ADB binary cannot be located')
         sys.exit(1)
 
+
+# Bail if ABIs do not match
+def assert_all_abis_match(devices):
+    # Use the first device as a baseline architecture and ensure all others match
+    base_device = devices[0]
+    base_device_abi = adb.abi(base_device)
+    for device in devices[1:]:
+        if adb.abi(device) != base_device_abi:
+            log.err('Device architectures do not all match')
+            sys.exit(1)
+
+
+# Bail if a device does not exist
+def assert_devices_exist(*device):
+    if None in device:
+        log.err(f'The specified transport id does not match any device')
+        sys.exit(1)
+
+
+# Bail if devices cannot be enumerated, return them if they can be
+def assert_devices():
     try:
         devices = adb.get_devices()
+
+        if len(devices) < 2:
+            log.err('Not enough devices connected; requires two or more')
+            sys.exit(1)
+
+        return devices
     except AttributeError:
         log.err('Failed to enumerate devices')
         sys.exit(1)
 
-    if len(devices) < 2:
-        log.err('Not enough devices connected; requires two or more')
+
+# Bail if user tried to use the receiver as the giver
+def assert_transport_ids(receiver_transport_id, giver_transport_id):
+    if receiver_transport_id is not None and receiver_transport_id == giver_transport_id:
+        log.err('The receiver cannot also be the giver')
         sys.exit(1)
 
+
+def migrate_all(devices, demo):
+    assert_all_abis_match(devices)
+    # Sync all devices with each other such that all package lists are identical
+    device_combos = list(itertools.combinations(devices, 2))
+    for device_pair in device_combos:
+        migrate.migrate(device_pair[0], device_pair[1], demo)
+        migrate.migrate(device_pair[1], device_pair[0], demo)
+
+
+def migrate_with_receiver_and_giver(receiver, giver, strict, demo):
+    assert_devices_exist(receiver, giver)
+    assert_all_abis_match([receiver, giver])
+    migrate.migrate(receiver, giver, demo)
+    if strict:
+        migrate.trim_excess(receiver, giver, demo)
+
+
+def migrate_with_receiver(devices, receiver, strict, demo):
+    assert_devices_exist(receiver)
+    assert_all_abis_match(devices)
+    _devices = devices
+    _devices.remove(receiver)
+    for device in _devices:
+        migrate.migrate(receiver, device, demo)
+        if strict:
+            migrate.trim_excess(receiver, device, demo)
+
+
+def migrate_with_giver(devices, giver, strict, demo):
+    assert_devices_exist(giver)
+    assert_all_abis_match(devices)
+    _devices = devices
+    _devices.remove(giver)
+    for device in _devices:
+        migrate.migrate(device, giver, demo)
+        if strict:
+            migrate.trim_excess(device, giver, demo)
+
+
+def main():
+    args.parse_args()
     receiver_transport_id = args.args.receiver
     giver_transport_id = args.args.giver
     strict = args.args.strict
     demo = args.args.demo
 
-    migrate = Migrate(
-        devices,
-        receiver_transport_id,
-        giver_transport_id,
-        strict,
-        demo
-    )
+    assert_adb_exists()
+    assert_transport_ids(receiver_transport_id, giver_transport_id)
+    devices = assert_devices()
 
-    if receiver_transport_id is not None and receiver_transport_id == giver_transport_id:
-        log.err('The receiver cannot also be the giver')
-        sys.exit(1)
+    receiver = adb.find_device_given_transport_id(devices, receiver_transport_id)
+    giver = adb.find_device_given_transport_id(devices, giver_transport_id)
 
+    # User did not specify a receiver or giver
     if receiver_transport_id is None and giver_transport_id is None:
         if strict:
             log.err('Receiver or giver must be explicitly set to use strict mode')
             sys.exit(1)
-        migrate.migrate_all()
+        migrate_all(devices, demo)
+
+    # User specified both a receiver and a giver
     elif receiver_transport_id is not None and giver_transport_id is not None:
-        migrate.migrate_with_receiver_and_giver()
+        migrate_with_receiver_and_giver(receiver, giver, strict, demo)
+
+    # User specified a receiver but not a giver
     elif receiver_transport_id is not None:
-        migrate.migrate_with_receiver()
+        migrate_with_receiver(devices, receiver, strict, demo)
+
+    # User specified a giver but not a receiver
     elif giver_transport_id is not None:
-        migrate.migrate_with_giver()
+        migrate_with_giver(devices, giver, strict, demo)
 
     log.dbg('Done')
+
+
+if __name__ == '__main__':
+    main()
--- src/migrate.py
+++ src/migrate.py
@@ -1,115 +1,35 @@
-import itertools
-import sys
 import adb
 import log
 import pm
 
 
-class Migrate:
-    def __init__(
-            self,
-            devices,
-            receiver_transport_id,
-            giver_transport_id,
-            strict=False,
-            demo=False
-    ):
-        self.devices = devices
-        self.receiver_transport_id = receiver_transport_id
-        self.giver_transport_id = giver_transport_id
-        self.strict = strict
-        self.demo = demo
+# Migrate packages from giver to receiver
+def migrate(receiving, giving, demo=False):
+    log.dbg(f'Syncing to {receiving.name} from {giving.name}')
 
-    def __sync(self, receiving, giving):
-        log.dbg(f'Syncing to {receiving.name} from {giving.name}')
+    receiving_pkg_ids = pm.parse_package_list(pm.get_packages(receiving))
+    giving_pkg_ids = pm.parse_package_list(pm.get_packages(giving, '-3'))
+    missing_pkg_ids = pm.diff_package_lists(giving_pkg_ids, receiving_pkg_ids)
 
-        receiving_pkg_ids = pm.parse_package_list(pm.get_packages(receiving))
-        giving_pkg_ids = pm.parse_package_list(pm.get_packages(giving, '-3'))
-        missing_pkg_ids = pm.diff_package_lists(giving_pkg_ids, receiving_pkg_ids)
+    if len(missing_pkg_ids) == 0:
+        log.warn('No missing packages to sync')
+    else:
+        for pkg_id in missing_pkg_ids:
+            log.dbg(f'Receiver missing: {pkg_id}')
+        if not demo:
+            pm.migrate_packages(receiving, giving, missing_pkg_ids)
 
-        if len(missing_pkg_ids) == 0:
-            log.warn('No missing packages to sync')
-        else:
-            for pkg_id in missing_pkg_ids:
-                log.dbg(f'Receiver missing: {pkg_id}')
-            if not self.demo:
-                pm.migrate_packages(receiving, giving, missing_pkg_ids)
 
-    def __trim(self, receiving, giving):
-        receiving_pkg_ids = pm.parse_package_list(pm.get_packages(receiving, '-3'))
-        giving_pkg_ids = pm.parse_package_list(pm.get_packages(giving))
-        excess_package_ids = pm.diff_package_lists(receiving_pkg_ids, giving_pkg_ids)
+# Trim excess packages from receiver that giver lacks
+def trim_excess(receiving, giving, demo=False):
+    receiving_pkg_ids = pm.parse_package_list(pm.get_packages(receiving, '-3'))
+    giving_pkg_ids = pm.parse_package_list(pm.get_packages(giving))
+    excess_package_ids = pm.diff_package_lists(receiving_pkg_ids, giving_pkg_ids)
 
-        if len(excess_package_ids) == 0:
-            log.warn('No excess packages to remove')
-        else:
-            for pkg_id in excess_package_ids:
-                log.dbg(f'[{receiving.name}] Receiver excess: {pkg_id}')
-                if not self.demo:
-                    adb.uninstall(receiving, pkg_id)
-
-    @staticmethod
-    def do_abis_match(devices):
-        # Use the first device as a baseline architecture and ensure all others match
-        base_device = devices[0]
-        base_device_abi = adb.abi(base_device)
-        for device in devices[1:]:
-            if adb.abi(device) != base_device_abi:
-                return False
-        return True
-
-    def assert_abis_match(self, devices):
-        if not self.do_abis_match(devices):
-            log.err('Device architectures do not all match')
-            sys.exit(1)
-
-    def migrate_all(self):
-        self.assert_abis_match(self.devices)
-        # Sync all devices with each other such that all package lists are identical
-        device_combos = list(itertools.combinations(self.devices, 2))
-        for device_pair in device_combos:
-            self.__sync(device_pair[0], device_pair[1])
-            self.__sync(device_pair[1], device_pair[0])
-
-    def migrate_with_receiver_and_giver(self):
-        receiver = adb.find_device_given_transport_id(self.devices, self.receiver_transport_id)
-        giver = adb.find_device_given_transport_id(self.devices, self.giver_transport_id)
-
-        if giver is None or receiver is None:
-            log.err(f'The specified transport id does not match any device')
-            sys.exit(1)
-
-        self.assert_abis_match([receiver, giver])
-        self.__sync(receiver, giver)
-        if self.strict:
-            self.__trim(receiver, giver)
-
-    def migrate_with_receiver(self):
-        receiver = adb.find_device_given_transport_id(self.devices, self.receiver_transport_id)
-
-        if receiver is None:
-            log.err(f'The specified transport id does not match any device')
-            sys.exit(1)
-
-        self.assert_abis_match(self.devices)
-        _devices = self.devices
-        _devices.remove(receiver)
-        for device in _devices:
-            self.__sync(receiver, device)
-            if self.strict:
-                self.__trim(receiver, device)
-
-    def migrate_with_giver(self):
-        giver = adb.find_device_given_transport_id(self.devices, self.giver_transport_id)
-
-        if giver is None:
-            log.err(f'The specified transport id does not match any device')
-            sys.exit(1)
-
-        self.assert_abis_match(self.devices)
-        _devices = self.devices
-        _devices.remove(giver)
-        for device in _devices:
-            self.__sync(device, giver)
-            if self.strict:
-                self.__trim(device, giver)
+    if len(excess_package_ids) == 0:
+        log.warn('No excess packages to remove')
+    else:
+        for pkg_id in excess_package_ids:
+            log.dbg(f'[{receiving.name}] Receiver excess: {pkg_id}')
+            if not demo:
+                adb.uninstall(receiving, pkg_id)
